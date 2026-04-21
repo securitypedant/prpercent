5a0470053501d71ba1c376fb59862a4b8ca0677b
Simon Thorpe <simon@thorpevillage.com>
2026-04-21 09:07:50 -0700

    Minor padding changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'passthrough',
+    platformProxy: {
+      enabled: false,
+    },
+  }),
 });
